2702575c2f132ecad6d0a709ea64f08add38dc30
Yuvraj <yuvrajbhatiofficial@gmail.com>
2026-05-06 00:19:03 +0530

    set domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://qylosai.vercel.app',
   integrations: [tailwind({
     applyBaseStyles: false, // We import globals.css manually
   }), react(), sitemap()]
